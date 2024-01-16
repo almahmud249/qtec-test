897999fb3ca45cf8780bb1cd482f92fd14a20f79
almahmud249 <almahmud.job49@gmail.com>
2024-01-16 17:12:54 +0600

    auth and role setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +19,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        //Disable foreign key checks
+        Schema::disableForeignKeyConstraints();
+
+        //1. Call the RolesAndPermissionsSeeder class
+        $this->call(RolesAndPermissionsSeeder::class);
+        //2. Call the UserSeeder class
+        $this->call(UserSeeder::class);
     }
 }
